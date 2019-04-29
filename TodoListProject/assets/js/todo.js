076e9a548662a239d202c6be4c81ab50f66adc65
suym7962018 <ys3587@nyu.edu>
2019-04-29 15:35:26 -0400

    Update todo.js
    marking completed

---
+++ TodoListProject/assets/js/todo.js
@@ -0,0 +1,25 @@
+//CHeck off specific todos by clicking
+
+// $("li").click(function(){
+// 	if($(this).css("color") === "rgb(128, 128, 128)"){
+// 		$(this).css({
+// 			color: "black",
+// 			textDecoration: "none"
+// 		});
+// 	}
+// 	else{
+// 		$(this).css({
+// 			color: "gray",
+// 			textDecoration: "line-through"
+// 		});
+// 	}
+// });
+
+$("li").click(function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("span").click(function(){
+
+});
